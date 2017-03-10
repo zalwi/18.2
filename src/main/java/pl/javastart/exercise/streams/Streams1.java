21c58543a295fdaa6911462250dd8a626bb75af4
Marcin Kunert <marcin.kunert@capgemini.com>
2017-03-10 18:31:57 +0100

    Added exercise about filtering

---
+++ src/main/java/pl/javastart/exercise/streams/Streams1.java
@@ -0,0 +1,10 @@
+package pl.javastart.exercise.streams;
+
+import java.util.List;
+
+public class Streams1 {
+
+    public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
+        return null;
+    }
+}
---
+++ src/test/java/pl/javastart/exercise/streams/Streams1Test.java
@@ -0,0 +1,38 @@
+package pl.javastart.exercise.streams;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Streams1Test {
+
+    private Streams1 streams = new Streams1();
+
+    @Test public void shouldFilterOutNotEvenNumbers() {
+
+        // given
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(1);
+        numberList.add(123);
+        numberList.add(11);
+        numberList.add(124);
+        numberList.add(12);
+        numberList.add(13);
+        numberList.add(21);
+        numberList.add(71);
+
+        // when
+        List<Integer> filteredList = streams.filterOutNotEvenNumbers(numberList);
+
+        // then
+        assertThat(filteredList.size(), is(2));
+        assertThat(filteredList, CoreMatchers.hasItem(12));
+        assertThat(filteredList, CoreMatchers.hasItem(124));
+    }
+
+}
