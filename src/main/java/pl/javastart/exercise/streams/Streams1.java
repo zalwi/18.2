30702051b1e19b265bdb00786437738d827be028
Marcin Kunert <marcin.kunert@capgemini.com>
2017-03-10 18:35:28 +0100

    Added second exercise

--- src/main/java/pl/javastart/exercise/streams/Streams1.java
+++ src/main/java/pl/javastart/exercise/streams/Streams1.java
@@ -7,4 +7,8 @@ public class Streams1 {
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
         return null;
     }
+
+    public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
+        return null;
+    }
 }
--- src/test/java/pl/javastart/exercise/streams/Streams1Test.java
+++ src/test/java/pl/javastart/exercise/streams/Streams1Test.java
@@ -35,4 +35,28 @@ public class Streams1Test {
         assertThat(filteredList, CoreMatchers.hasItem(124));
     }
 
+    @Test public void shouldFilterOutNumberLowerThan20() {
+
+        // given
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(1);
+        numberList.add(123);
+        numberList.add(11);
+        numberList.add(124);
+        numberList.add(12);
+        numberList.add(13);
+        numberList.add(21);
+        numberList.add(71);
+
+        // when
+        List<Integer> filteredList = streams.filterOutLowerNumbersThan20(numberList);
+
+        // then
+        assertThat(filteredList.size(), is(4));
+        assertThat(filteredList, CoreMatchers.hasItem(123));
+        assertThat(filteredList, CoreMatchers.hasItem(124));
+        assertThat(filteredList, CoreMatchers.hasItem(21));
+        assertThat(filteredList, CoreMatchers.hasItem(71));
+    }
+
 }
