6e67c16e457c1dea5be75d8372078008b00b8126
Kamil Zalwert <kamilz89@gmail.com>
2020-06-08 16:29:37 +0200

    18.2 - FilteringMachineTwo poprawione

--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -1,15 +1,21 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachineTwo {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
-        return null;
+        return ppl.stream()
+                .filter(person -> person.getAge() < 18)
+                .map(Person::getName)
+                .collect(Collectors.toList());
     }
 
     public List<User> convertPeopleToUsers(List<Person> people) {
-        return null;
+        return people.stream()
+                .map(person -> new User(person.getName(), person.getAge(), (person.getName()+"_"+person.getAge())))
+                .collect(Collectors.toList());
     }
 }
